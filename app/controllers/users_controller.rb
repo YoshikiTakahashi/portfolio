ae2c8dad9dbe4ba03605244f3840a7bb6373bb30
Yoshiki Takahashi <ysk.33.ysk@gmail.com>
2021-03-06 22:05:01 +0900

    Change Users to implement signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render '/users/new'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe "users/new", type: :feature do
-  describe "#new" do
+RSpec.describe "Users", type: :feature do
+  describe "GET /new" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com")
     end
-
-    
   end
 end
