878f96ad054aa61983792c7e37d39f7f7377e70a
Yoshiki Takahashi <ysk.33.ysk@gmail.com>
2021-03-06 11:43:14 +0900

    Added User routing, set model and db. And secure password.

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe "users/new", type: :feature do
+  describe "#new" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com")
+    end
+
+    
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,3 @@
-# RSpec.describe "home_pages/home.html.erb", type: :view do
+# RSpec.describe User, type: :model do
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
