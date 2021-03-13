08629d8f3db67b96f2ceb312bfe4335cafa630c4
Yoshiki Takahashi <ysk.33.ysk@gmail.com>
2021-03-14 00:48:39 +0900

    Do not use singup enablement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+# account_activation
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     @user.send_activation_email
+  #     flash[:info] = "Please check your email to activate your account."
+  #     redirect_to root_url
+  #   else
+  #     render 'new'
+  #   end
+  # end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :reset_token
   before_save   :downcase_email
-  before_create :create_activation_digest
+  # before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
@@ -65,8 +65,8 @@ class User < ApplicationRecord
     self.email = email.downcase
   end
 
-  def create_activation_digest
-    self.activation_token  = User.new_token
-    self.activation_digest = User.digest(activation_token)
-  end
+  # def create_activation_digest
+  #   self.activation_token  = User.new_token
+  #   self.activation_digest = User.digest(activation_token)
+  # end
 end
