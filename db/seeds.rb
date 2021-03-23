65402f4036c9b51eb6c83624d58331ddb4a400bd
Yoshiki Takahashi <ysk.33.ysk@gmail.com>
2021-03-23 20:46:56 +0900

    Reset the data and change the seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+User.create!(name:  "guest",
+             email: "guest@guest.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
 
-20.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -14,7 +14,7 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
-20.times do |n|
+5.times do |n|
   company = Faker::Company.name
   name = Faker::Name.name
   phone = Faker::PhoneNumber.cell_phone
